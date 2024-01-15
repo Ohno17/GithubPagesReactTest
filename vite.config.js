e881d9276301bd919519f4b640b31833869a525c
Ohno17 <107664053+Ohno17@users.noreply.github.com>
2024-01-15 23:44:21 +0000

    final fix vite config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+	base: 'https://ohno17.github.io/GithubPagesReactTest/',
   server: {
-		base: '/GithubPagesReactTest/dist/',
     host: '0.0.0.0',
   }
 })
